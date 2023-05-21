6e52790c0994ee5fbbba5e121b96a4cc6d1a1902
Flavio de Lima <flaviolimpio@gmail.com>
2023-05-20 21:42:03 -0300

    create board and cells presentational components

---
+++ src/components/Board.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import Cell from './Cell.vue'
+
+defineProps<{
+  cells: boolean[][];
+}>()
+</script>
+
+<template>
+  <div id="board">
+    <div v-for="row , rowNumber in cells" :key="rowNumber" class="rows">
+      <div v-for="cell, columnNumber in row" :key="columnNumber">
+        <Cell
+          class="cell"
+          :class="{ alive: cell }"
+          :alive="cell"
+          :position="{ x:columnNumber, y:rowNumber }"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+#board {
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: repeat(3, 1fr);
+  grid-gap: 1rem;
+  height: 100%;
+  aspect-ratio: 1 / 1;
+  margin: 0 auto;
+  /* background-color: blanchedalmond; */
+}
+
+.rows {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 1rem;
+  /* background-color: blanchedalmond; */
+}
+</style>
---
+++ src/components/Cell.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+defineProps<{
+  position: { x: number; y: number };
+  alive: boolean;
+}>()
+</script>
+
+<template>
+  <div
+    id="cell"
+    :class="{ alive }"
+  ></div>  
+</template>
+
+<style scoped>
+#cell {
+  width: 100%;
+  height: 100%;
+  background-color: rgb(56, 44, 44);
+}
+
+#cell.alive {
+  background-color: rgb(207, 191, 191);
+}
+</style>
